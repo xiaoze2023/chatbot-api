ea55329b7ad396ef2cc83597a567f6b0b2e65442
xiaoze2023 <315626261@qq.com>
2024-06-18 13:18:17 +0800

    小泽:初始化工程DDD

--- chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -1,6 +1,23 @@
 package chatbot.api.test;
 
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
+import java.io.Closeable;
+import java.net.HttpCookie;
+
 //单元测试
 public class ApiTest {
+    @Test
+    public void query_unanswered_questions(){
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("");
+
+    }
+
+
 }
