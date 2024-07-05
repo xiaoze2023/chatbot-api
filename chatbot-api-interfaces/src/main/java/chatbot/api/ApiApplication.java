7adeafce3287a5cbf39584bfe57c526cff67e75f
xiaoze2023 <315626261@qq.com>
2024-07-05 17:28:27 +0800

    小泽:整合chatGPT和知识星球

--- chatbot-api-interfaces/src/main/java/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/chatbot/api/ApiApplication.java
@@ -2,6 +2,7 @@ package chatbot.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 //启动类
 @SpringBootApplication
